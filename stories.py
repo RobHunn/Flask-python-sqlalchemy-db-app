ddcc486118ae088b4c9230c36b110d0057a6b139
RobHunn <robert.hunnicutt@cobrakaicoding.com>
2020-04-15 11:55:12 -0400

    added new story, fixed typo on base.html, css bug

--- stories.py
+++ stories.py
@@ -57,6 +57,81 @@ story2 = Story(
     ["noun", "verb"],
     """OMG!! OMG!! I love to {verb} a {noun}!"""
 )
+story3 = Story(
+    "date",
+    "How to Date the Coolest Guy/Girl in School",
+    [
+        "plural_noun",
+        "adverb",
+        "verb",
+        "article_of_clothing",
+        "body part",
+        "adjective3",
+        "noun",
+        "another_body_part",
+        "plural_noun",
+        "another_body_part2",
+        "noun2",
+        "verb_ending_in_ing",
+        "adjective",
+        "adjective2",
+        "verb2"
+    ],
+    """ 
+    
+          It's simple. Turn the {plural_noun}. Make him/her want {adverb} 
+        to date you. Make sure you're always dressed to {adjective}. Each and 
+        every day, wear a/an {article_of_clothing} that you know shows off 
+        your {noun2} to advantage and make your {verb} look like a million {noun}.
+        
+        Even if the two of you make meaningful contact, don't admit it. No hugs or 
+        {verb_ending_in_ing}. Just shake his/her {another_body_part} firmly. And 
+        remember, when he/she asks you out, even though a chill may run down your 
+        {another_body_part2} and you can't stop your {verb2} from {adjective}, 
+        just play it {adjective2}. Take a long pause before answering in a very 
+        {adjective3} voice. I'll have to {plural_noun} it over.  
+
+
+    """
+)
+
+story4 = Story(
+    "pirate",
+    "Talk Like a Pirate",
+    [
+        "noun",
+        "adjective",
+        "verb",
+        "adverb",
+        "noun2",
+        "adjective2",
+        "plural_noun",
+        "part_of_the_body",
+        "noun3",
+        "noun4",
+        "part_of_the_body2"
+    ],
+    """
+    
+          Ye can always pretend to be a bloodthirsty {noun},
+        threatening everyone by waving yer sword in the air, 
+        but until ye learn to {adjective}, like a pirate, 
+        ye'll never be {adjective2}, accepted as an authentic {verb}.
+
+        So here's what ye do: Cleverly work into yer daily conversations,
+        pirate phrases such as "Ahoy there, {adverb}", "Avast, ye {noun2}", 
+        and "Shiver me {adjective2}". Remember to drop all yer gs when ye say 
+        such words as sailin', spittin', and fightin'. This will give ye a/an 
+        {plural_noun}, start to being recognized as a swashbucklin'. Once ye 
+        have the lingo down pat, it helps to wear a three-cornered {noun3},
+        on yer head, stash a/an {part_of_the_body}, in yer pants, and keep 
+        a/an {part_of_the_body2}, perched atop yer{noun4}. 
+        
+        Aye, now ye be a real pirate!
+        
+        
+        """
+)
 
 # Make dict of {code:story, code:story, ...}
-stories = {s.code: s for s in [story1, story2]}
+stories = {s.code: s for s in [story1, story2, story3, story4]}
